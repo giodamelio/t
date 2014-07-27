b2486f27a4596eead9e7e15cbd0db4d94d630328
Gio d'Amelio <giodamelio@gmail.com>
2014-07-27 13:54:08 -0700

    Basic argparse options

--- t.py
+++ t.py
@@ -1,3 +1,32 @@
-#!/usr/bin/python
-print("Hello World!")
+#!/usr/bin/python3
+import argparse
+
+# Parse the arguments
+parser = argparse.ArgumentParser(description="t, The simple tmux helper")
+subparsers = parser.add_subparsers(dest="command")
+
+# List
+checkout = subparsers.add_parser("list",
+        aliases=["ls"],
+        help="List sessions")
+
+# Attach
+attach = subparsers.add_parser("attach",
+        aliases=["a"],
+        help="Attach to session by name")
+attach.add_argument("session",
+        metavar="session",
+        help="Session name")
+
+# Remove
+remove = subparsers.add_parser("remove",
+        aliases=["rm"],
+        help="Delete to session by name")
+remove.add_argument("session",
+        metavar="session",
+        help="Session name")
+
+
+args = parser.parse_args()
+print(args)
 
